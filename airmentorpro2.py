e02b696077b81236e65b6469bc7888f7201ee13a
philippeportesppo <philippeportesppo@users.noreply.github.com>
2017-03-11 19:08:20 -0800

    Updated python and php script to fix temp/hum
    Updated the temperature/humidity extraction based on AirMentorPro actual computations.
    Added the compensated temperature to the php page too. This will be used by the DTH as temperature instead of "real temperature".

--- airmentorpro2.py
+++ airmentorpro2.py
@@ -15,17 +15,9 @@
 #   limitations under the License.
 import sys
 import requests
+import math
 from bluepy.btle import Scanner, DefaultDelegate
 
-AMP_CO2=0
-AMP_PM25=0
-AMP_PM10=0
-AMP_TVOC=0
-AMP_HUM=0
-AMP_TEM=0
-AMP_IAQ=0
-AMP_BATT=0
-AMP_CHARG=0
  
 class AirMentorProDelegate(DefaultDelegate):
     def __init__(self, bluetooth_adr):
@@ -35,11 +27,12 @@ class AirMentorProDelegate(DefaultDelegate):
 	self.AMP_PM25=0
 	self.AMP_PM10=0
 	self.AMP_TVOC=0
-	self.AMP_HUM=0
-	self.AMP_TEM=0
+	self.AMP_HUM=0.0
+	self.AMP_TEM=0.0
 	self.AMP_IAQ=0
 	self.AMP_BATT=0
 	self.AMP_CHARG=0
+        self.AMP_TEMP_CAL=0.0
         DefaultDelegate.__init__(self)
 
     def handleDiscovery(self, dev, isNewDev, isNewdata):
@@ -50,38 +43,37 @@ class AirMentorProDelegate(DefaultDelegate):
             	if scan[0] == 0xff: # Proprietary
 	            payload = {}
 			      
-                    if int(scan[2][0:4],16)==0x2221:			      
+                    if int(scan[2][1:3],16)==0x22:			      
                         print scan,
 			hex_data = scan[2]     
-		        #      [TVOC][Temp][Humi][IAQ ]
-			#[2221][00b9][1963][332d][0145]			      
+		        #      [TVOC][Temp][TempDelta][Humi][IAQ ]
+			#[2221][00b9][1963][33       ][  2d][0145]			      
                    
 			self.AMP_TVOC=int(hex_data[4:8],16)
-                   	print "AMP_TVOC",self.AMP_TVOC,
+                   	print "AMP_TVOC: ",self.AMP_TVOC,
 
-	                self.AMP_TEM=(int(hex_data[8:12],16)-0x11DE)/100.0
+	                self.AMP_TEM=(float(int(hex_data[8:12],16))-4000)*0.01
 		   	print "AMP_TEM: ",self.AMP_TEM,
 
-		   	self.AMP_HUM=(int(hex_data[12:16],16)-0x221B)/100.0  #22210095[1968][392a][0023] 59%
-		   	                                                     
-		   	                                                     #222100ec[1870][0c2a][0025] 45%
-                   	print "AMP_HUM: ",self.AMP_HUM,                      #222100ee[1866][0c2b][0026] 46%
-                   	
-                   							     #222100f2[1861][0c2d][0026] 48%
+                        self.AMP_TEMP_CAL=self.AMP_TEM-(float(int(hex_data[12:14],16)))*0.1
+			print "AMP_TEMP_CAL: ",self.AMP_TEMP_CAL,
 
+		   	self.AMP_HUM=float(int(hex_data[14:16],16))*math.exp(self.AMP_TEM*17.62/(self.AMP_TEM+243.12))/math.exp(self.AMP_TEMP_CAL*17.62/(self.AMP_TEMP_CAL+243.12))     
+                   	print "AMP_HUM: ",self.AMP_HUM,
+                   	
  			self.AMP_IAQ=int(hex_data[16:],16)
-                        print "AMP_IAQ",self.AMP_IAQ
+                        print "AMP_IAQ: ",self.AMP_IAQ
 
 		    	try:
-	                   requests.get("http://localhost/airmentorpro2.php?Action=set&CO2="+str(self.AMP_CO2)+"&PM25="+str(self.AMP_PM25)+"&PM10="+str(self.AMP_PM10)+"&TEM="+str(self.AMP_TEM)+"&HUM="+str(self.AMP_HUM)+"&TVOC="+str(self.AMP_TVOC)+"&IAQ="+str(self.AMP_IAQ)+"&BATT="+str(self.AMP_BATT), data=payload) 
+	                   requests.get("http://localhost/airmentorpro2.php?Action=set&CO2="+str(self.AMP_CO2)+"&PM25="+str(self.AMP_PM25)+"&PM10="+str(self.AMP_PM10)+"&TEM_ACT="+str(self.AMP_TEM)+"&TEM_CAL="+str(self.AMP_TEMP_CAL)+"&HUM="+str(self.AMP_HUM)+"&TVOC="+str(self.AMP_TVOC)+"&IAQ="+str(self.AMP_IAQ)+"&BATT="+str(self.AMP_BATT), data=payload) 
 			except:
                        	   print "Couldn't send request..."
                                                    
 		    else:
-			if int(scan[2][0:4],16)==0x2121:
+			if int(scan[2][1:3],16)==0x12:
 			    hex_data = scan[2]      
-			    #       [CO2 ][PM25][PM10]
-			    # [2121][2710][0003][0003]0000
+			    #       [CO2 ][PM25][PM10][CO O3]
+			    # [2121][2710][0003][0003][00 00]
                             print scan,
 			    self.AMP_CO2=int(hex_data[4:8],16)
 		            print "AMP_CO2",self.AMP_CO2,
@@ -95,7 +87,7 @@ class AirMentorProDelegate(DefaultDelegate):
                             #AMP_BATT=int(hex_data[16],16)
                             #print "self.AMP_BATT",self.AMP_BATT,
                             try:
-	                        requests.get("http://localhost/airmentorpro2.php?Action=set&CO2="+str(self.AMP_CO2)+"&PM25="+str(self.AMP_PM25)+"&PM10="+str(self.AMP_PM10)+"&TEM="+str(self.AMP_TEM)+"&HUM="+str(self.AMP_HUM)+"&TVOC="+str(self.AMP_TVOC)+"&IAQ="+str(self.AMP_IAQ)+"&BATT="+str(self.AMP_BATT), data=payload) 
+	                        requests.get("http://localhost/airmentorpro2.php?Action=set&CO2="+str(self.AMP_CO2)+"&PM25="+str(self.AMP_PM25)+"&PM10="+str(self.AMP_PM10)+"&TEM_ACT="+str(self.AMP_TEM)++"&TEM_CAL"+str(self.AMP_TEMP_CAL)+"&HUM="+str(self.AMP_HUM)+"&TVOC="+str(self.AMP_TVOC)+"&IAQ="+str(self.AMP_IAQ)+"&BATT="+str(self.AMP_BATT), data=payload) 
 			    except:
                        	        print "Couldn't send request..."
                 #else:

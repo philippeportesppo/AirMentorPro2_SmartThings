013ecd3c968170915497a25dbc0378835a185f5e
PPO16 <philippeportesppo@users.noreply.github.com>
2018-02-12 19:13:54 -0800

    Fixed early execution network adapter identification failure
    Now can be launched from /etc/rc.local

--- ssdp_upnp/ssdp_server.py
+++ ssdp_upnp/ssdp_server.py
@@ -2,7 +2,7 @@
 #Copyright (c) 2016 Erwan Martin <public@fzwte.net>
 #Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 #The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 # adapter by Philippe Portes 2018 from orignal ssdp_server.py written by ZeWarren on https://github.com/ZeWaren/python-upnp-ssdp-example/blob/master/__main__.py
 
@@ -23,7 +23,7 @@ from time import sleep
 
 
 
-NETWORK_INTERFACE = 'wlan0' # or 'eth0'
+NETWORK_INTERFACE = 'wlan0'
 
 
 
@@ -76,25 +76,28 @@ def get_network_interface_ip_address(interface='wlan0'):
 
     """
 
+
+    while interface not in ni.interfaces():
+
+        print('Could not find interface %s.' % (interface,))
+
+        sleep(10)
+
+
+
     while True:
+        interfacestring = ni.ifaddresses(interface)
 
-        if NETWORK_INTERFACE not in ni.interfaces():
-
-            print('Could not find interface %s.' % (interface,))
-
-            exit(1)
-
-        interface = ni.ifaddresses(interface)
-
-        if (2 not in interface) or (len(interface[2]) == 0):
+        if (2 not in interfacestring) or (len(interfacestring[2]) == 0):
 
             print('Could not find IP of interface %s. Sleeping.' % (interface,))
 
-            sleep(60)
+            sleep(10)
 
             continue
-
-        return interface[2][0]['addr']
+        else:
+            break
+    return interfacestring[2][0]['addr']
 
 
 print "Start..."
